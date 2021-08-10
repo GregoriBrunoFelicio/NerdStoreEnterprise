f202c7e746d4f35382bef13a328ffb6459ab6da7
gregori bruno felicio <gregory.wow@hotmail.com>
2021-08-10 20:39:44 -0300

    cria registro e login view

--- src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
+++ src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -1,8 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models;
+using System.Threading.Tasks;
 
 namespace NSE.WebApp.MVC.Controllers
 {
     public class IdentidadeController : Controller
     {
+        [HttpGet("nova-conta")]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost("nova-conta")]
+        public async Task<IActionResult> Registro(UsuarioRegistro usuarioRegistro)
+        {
+            return View();
+        }
+
+        [HttpGet("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Registro(UsuarioLogin usuarioLogin)
+        {
+            return View();
+        }
+
+        [HttpGet("sair")]
+        public async Task<IActionResult> Logout()
+        {
+            return View();
+        }
     }
 }
